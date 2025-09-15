c1fb609e8d32d3ba298d26d8d803243f909ad5e2
nav-iq <naveedh10565@gmail.com>
2025-09-15 18:05:07 +0500

    fixed module issue

--- src/middlewares/error.middleware.js
+++ src/middlewares/error.middleware.js
@@ -1,4 +1,4 @@
-import { ApiError } from "../utils/apiError.js";
+import { ApiError } from "../utils/ApiError.js";
 
 // Global error handling middleware
 const errorHandler = (err, req, res, next) => {
--- src/services/email.service.js
+++ src/services/email.service.js
@@ -1,5 +1,5 @@
 import nodemailer from "nodemailer";
-import { ApiError } from "../utils/apiError.js";
+import { ApiError } from "../utils/ApiError.js";
 import { Validator } from "../utils/validator.js";
 import {
   generateWaitlistNotificationTemplate,
