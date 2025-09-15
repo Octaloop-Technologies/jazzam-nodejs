b2ce65b5f407de1d57cf7e81de5abf32f9381b02
nav-iq <naveedh10565@gmail.com>
2025-09-15 17:32:38 +0500

    error module fixed

--- src/middlewares/error.middleware.js
+++ src/middlewares/error.middleware.js
@@ -1,4 +1,4 @@
-import { ApiError } from "../utils/ApiError.js";
+import { ApiError } from "../utils/apiError.js";
 
 // Global error handling middleware
 const errorHandler = (err, req, res, next) => {
