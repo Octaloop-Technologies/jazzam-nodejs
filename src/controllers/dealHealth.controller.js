6dbfdbbaac239b1a46e76766cc3fc17e885a03c9
mudassar <mudassarr640@gmail.com>
2025-12-02 17:38:57 +0500

    added new branch

--- src/controllers/dealHealth.controller.js
+++ src/controllers/dealHealth.controller.js
@@ -35,19 +35,19 @@ const getLeadHealth = asyncHandler(async (req, res) => {
 });
 
 // Get dashboard metrics
-const getDashboardMetrics = asyncHandler(async (req, res) => {
-  const metrics = await dealHealthService.getDashboardMetrics(req.company._id);
+// const getDashboardMetrics = asyncHandler(async (req, res) => {
+//   const metrics = await dealHealthService.getDashboardMetrics(req.company._id);
 
-  return res
-    .status(200)
-    .json(
-      new ApiResponse(
-        200,
-        metrics,
-        "Dashboard metrics retrieved successfully"
-      )
-    );
-});
+//   return res
+//     .status(200)
+//     .json(
+//       new ApiResponse(
+//         200,
+//         metrics,
+//         "Dashboard metrics retrieved successfully"
+//       )
+//     );
+// });
 
 // Get engagement history for a lead
 const getEngagementHistory = asyncHandler(async (req, res) => {
